497a3fe6640dbb01fd04b9fcb190835299910e66
Praveen Kumar <107540493+praveenWebDesigner360@users.noreply.github.com>
2022-07-06 13:04:22 +0530

    initial commit

---
+++ src/AppNew.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./App.css";
+import Form from "./components/Form";
+
+class AppNew extends Component {
+  render() {
+    return (
+      <div className="AppNew">
+        <Form />
+      </div>
+    );
+  }
+}
+
+export default AppNew;

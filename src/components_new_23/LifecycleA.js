35b8c8a3b9340aa5491515585c6ce627dfe09130
Praveen Kumar <107540493+praveenWebDesigner360@users.noreply.github.com>
2022-07-08 15:17:18 +0530

    initial commit

---
+++ src/components_new_23/LifecycleA.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class LifecycleA extends React.Component {
+  render() {
+    return (
+      <div>
+        <div></div>
+      </div>
+    );
+  }
+}
+
+export default LifecycleA;
